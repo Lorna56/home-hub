9c84b00d6fea355eae19c6e15b8cacb1db51ce40
Lorna56 <your-github-lornanaula0042@gmail.com>
2026-07-20 17:51:32 +0300

    Creayed users model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        LANDLORD = "LANDLORD", "Landlord"
+        TENANT = "TENANT", "Tenant"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.TENANT
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.username
