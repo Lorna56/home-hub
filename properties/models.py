20da10c8b6d03a0e979bbd9905032046863c0099
Lorna56 <lornanaula0042@gmail.com>
2026-07-23 16:00:44 +0300

    Added model logic for properties app

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Property(models.Model):
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="properties"
+    )
+
+    name = models.CharField(
+        max_length=255
+    )
+
+    description = models.TextField(
+        blank=True
+    )
+
+    address = models.CharField(
+        max_length=255
+    )
+
+    location = models.CharField(
+        max_length=255
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+
+    def __str__(self):
+        return self.name
